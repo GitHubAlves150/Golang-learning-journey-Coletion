be22a88d02871eb37ad4f71d9bce0a1d112b0549
Lucas Alves <senoratec.io@gmail.com>
2026-05-02 17:20:00 -0300

    Básico de laço for com slice e maps

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 /*
 *Sintaxe básica
-*/
+ */
 
 package main //declara o pactoe main
 
@@ -8,8 +8,37 @@ package main //declara o pactoe main
 import (
 	"fmt"
 )
-func main(){
 
-	fmt.Println("ola...");
+func main() {
+
+	//laço for simples
+	for i := 0; i < 10; i++ {
+		fmt.Println(">> ", i)
+	}
+	//range
+	numbers := []int{10, 20, 33, 43}
+
+	for indices, _ := range numbers {
+		fmt.Println("indice: ", indices)
+		//fmt.Println("valorres: ", valores)
+
+	}
+
+	capitais := map[string]string{
+		"Br" : "Brasilia", //chave - valor
+		"Fr" : "Paris",
+		"Jp" : "Tóquio",
+	}
+
+	for pais, capital:= range capitais{
+		fmt.Println("Pais: ", pais)
+		fmt.Println("Capital :", capital)
+	}
+
+
+
+	fmt.Println("ola...")
 }
+
+
 
