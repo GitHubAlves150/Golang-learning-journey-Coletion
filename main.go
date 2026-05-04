80d3d4e93532b33a23ae1ae4a74669fc5e11b8bd
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-04 17:01:54 -0300

    Update main.go

--- main.go
+++ main.go
@@ -11,34 +11,39 @@ import (
 
 func main() {
 
-	//laço for simples
-	for i := 0; i < 10; i++ {
-		fmt.Println(">> ", i)
+	//O que é slice?
+	//arrays mais flexiveis
+
+	var numbers = [6]int{2, 54, 66} //com tamanho é array e sem tamanho é slice
+	var numeros = []int{12, 44, 65} //slice - simples e mágico (uuui)
+	fmt.Println(numbers)
+	fmt.Println(numeros)
+
+	days := []string{"Segunda", "Terça", "Quarta"}
+	fmt.Println(days)
+	days = append(days, "Quinta")
+
+	fmt.Println(days)
+	numeros = append(numeros, 2)
+	fmt.Println(numeros)
+
+	vetorNumeros := []int{2, 3, 12, 21, 342, 234}
+
+	fmt.Println(vetorNumeros[:2])
+	vetorNumeros = append(vetorNumeros[:2], vetorNumeros[3:]...)
+	fmt.Println(vetorNumeros)
+
+	matriz := [][]int{
+		{1, 2, 3},
+		{5, 6, 7},
+		{7, 8, 9},
 	}
-	//range
-	numbers := []int{10, 20, 33, 43}
 
-	for indices, _ := range numbers {
-		fmt.Println("indice: ", indices)
-		//fmt.Println("valorres: ", valores)
+	fmt.Println(matriz)
 
-	}
+	numbers2 := make([]int, 5) //cria um slice de 5 posições de forma dinâmica -comprimento  e capacidade. Ou seja make([]int, comprimento, capacidade)
 
-	capitais := map[string]string{
-		"Br" : "Brasilia", //chave - valor
-		"Fr" : "Paris",
-		"Jp" : "Tóquio",
-	}
-
-	for pais, capital:= range capitais{
-		fmt.Println("Pais: ", pais)
-		fmt.Println("Capital :", capital)
-	}
-
-
-
+	fmt.Println(cap(numbers2))
+	fmt.Println(len(numbers2))
 	fmt.Println("ola...")
 }
-
-
-
