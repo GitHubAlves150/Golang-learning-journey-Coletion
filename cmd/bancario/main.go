c7db3f617c9bc4194a5c3ba77c5656f402ce3c9f
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-06 14:52:33 -0300

    Paerte do banco finalizada - entender o mecanismo em breve

--- cmd/bancario/main.go
+++ cmd/bancario/main.go
@@ -1,33 +1,54 @@
-/*
-*Você foi contratado para implementar um sistema bancário que
- gerencia contas correntes. O sistema deve usar métodos
- (com receivers por valor e por ponteiro) para manipular as contas.
+package main
 
- =======================================================================
- Objetivos do Exercício
-
-    ✅ Praticar métodos com receiver por valor (func (c Conta))
-
-    ✅ Praticar métodos com receiver por ponteiro (func (c *Conta))
-
-    ✅ Entender quando usar valor vs ponteiro
-
-    ✅ Implementar validações e tratamento de erros
-
-    ✅ Trabalhar com múltiplos tipos e interações entre objetos
-*/
-
-package main //declara o pactoe main
-
-//importa o pacote fmt para usar a funão println
 import (
+	"bancario/internal/banco"
+
 	"fmt"
-	"github.com/GitHubAlves150/Golang-learning-journey-Coletion.git/internal/conta"
 )
 
 func main() {
 
-	minha_conta := conta.NovaConta(2344, "Lucas")
-	
-	fmt.Println("\nfim...")
+	//minhaConta := conta.Conta{}
+
+	//===========================================
+	//criar um novo banco
+	//===========================================
+	meuBanco := banco.NovoBanco("Banco do Brasil")
+	fmt.Println("Bnaco: ", meuBanco.GetNome())
+
+	//===========================================
+	//Abrir uma conta (método do banco)
+	//===========================================
+	conta1 := meuBanco.AbrirConta("Lucas Alves")
+	fmt.Println("conta1", conta1)
+
+	conta2 := meuBanco.AbrirConta("Maria Azevedo")
+	fmt.Println("conta2", conta2)
+
+	conta3 := meuBanco.AbrirConta("Constâncio Branito")
+	fmt.Println("conta3", conta3)
+
+	//Lucas Abre uma segunda conta
+	conta4 := meuBanco.AbrirConta("Lucas Alves")
+	fmt.Println("conta4", conta4)
+
+	// ========================================
+	// 3. REALIZAR OPERAÇÕES
+	// ========================================
+	fmt.Println("Deposito.....................")
+	err :=conta1.Deposito(12.90)
+	if err == nil {
+		fmt.Println("Depositado......")
+	}else{
+		fmt.Println("Não foi possivel depositar")
+	}
+
+	fmt.Println("saldo: ",conta1)
+
+
+
+	//===========================================
+	//Exibir contas
+	//===========================================
+
 }
