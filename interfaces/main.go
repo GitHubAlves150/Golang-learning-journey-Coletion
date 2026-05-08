f6092313f8c30741107b8b08f3e60a9c3d8446ab
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-08 14:08:23 -0300

    Exelente exemplo sobre thread/concorrencia

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,11 +1,38 @@
 package main
 
-import(
+import (
 	"fmt"
+	"time"
 )
+
+
+func FazerCafe(){
+	fmt.Println("Fazendo café")
+	time.Sleep(3*time.Second)
+	fmt.Println("Café pronto")
+}
+
+func FritarOvos(){
+	fmt.Println("Fritando Ovos")
+	time.Sleep(3*time.Second)
+	fmt.Println("ovos prontos")
+}
+
+func FazerTorrada(){
+	fmt.Println("FazerTorrada")
+	time.Sleep(3*time.Second)
+	fmt.Println("Torrada Pronta")
+}
+
 
 func main() {
 
-	
+	start := time.Now()
+	go FazerCafe()
+	go FritarOvos()
+	go FazerTorrada()
+
+	time.Sleep(3* time.Second)
+fmt.Println( time.Since(start))
 	fmt.Println("....FIM.....")
 }
