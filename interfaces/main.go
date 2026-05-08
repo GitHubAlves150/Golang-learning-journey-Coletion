2d2d89cefcea4cbb9226b5dbd1586d240e1d0508
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-08 12:42:15 -0300

    Neste primeira etapa, está sendo usado apenas interface e não polimorfismo. Na próxima branch será usado uma função finalizarPagamento que aceita as 3 formas de pagamento

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,26 +1,96 @@
+//=================================================================================================
+//Com apenas interface
+/*
+|
+|
+|
+|
+|
+*/
 package main
 
 import (
 	"fmt"
-
-	"github.com/GitHubAlves150/Golang-learning-journey-Coletion.git/interfaces/datasource"
 )
 
-//import "fmt"
+// =================================================================================================
+// 1. A interface (O contrato)
+// =================================================================================================
+type Pagamento interface {
+	Pagar(valor float64) string
+	GetNome() string
+}
+
+//=================================================================================================
+//2. Cartão (implementa o pagamento)
+//=================================================================================================
+
+type Cartao struct {
+	NomeTitular string
+	Limite      float64
+}
+
+func (c Cartao) Pagar(valor float64) string {
+	if valor < c.Limite {
+		return "Cartão recusado: limite insuficiente"
+	}
+	return fmt.Sprintf("Pagamento de R$ %.2f aprovado no cartão de %s", valor, c.NomeTitular)
+}
+
+func (c Cartao) GetNome() string {
+	return "Cartão"
+}
+
+//=================================================================================================
+//3. Pix (implementa o pagamento)
+//=================================================================================================
+
+type Pix struct {
+	Chave string
+	Saldo float64
+}
+
+func (p Pix) Pagar(valor float64) string {
+	if valor < p.Saldo {
+		return "Saldo insuficiente:"
+	}
+	return fmt.Sprintf("Pagamento de R$ %.2f aprovado na chave de %s", valor, p.Chave)
+}
+
+func (p Pix) GetNome() string {
+	return "Pix"
+}
+
+//=================================================================================================
+//4. Boleto (implementa o pagamento)
+//=================================================================================================
+
+type Boleto struct {
+	codigo string
+}
+
+func (b Boleto) Pagar(valor float64) string {
+
+	return fmt.Sprintln("Boleto gerado com sucesso. Numero:", b.codigo[:10])
+}
+
+func (p Boleto) GetNome() string {
+	return "Boleto"
+}
 
 func main() {
 
-	datasource := datasource.NewPostgreSQL("localhost", "teste de polimorfismo", 9)
+	cartao := Cartao{NomeTitular: "Lucas", Limite: 2000000.0}
+	pix := Pix{Chave: "048988450724", Saldo: 3000.0}
+	boleto := Boleto{codigo: "AHSTFR654389FDSJFSDFKDMM998880-9"}
 
-	saveNewname(&datasource, "LucasDb")
-	saveNewname(&datasource, "AlvesDb")
-	GetNames(&datasource)
-}
+	a := Pagamento.Pagar(cartao, 3000000)
+	b := Pagamento.Pagar(pix, 40000)
+	c := Pagamento.Pagar(boleto, 30)
 
-func saveNewname(d datasource.Datasource, name string) {
-	d.Save(name)
-}
+	fmt.Println("..", a)
+	fmt.Println("..", b)
+	fmt.Println("..", c)
 
-func GetNames(d datasource.Datasource) {
-	fmt.Println(d.GetAll())
+	fmt.Println("....FIM.....")
 }
