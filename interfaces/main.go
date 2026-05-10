98a48265d91c92d3b4fc4b2d0d9ee345e4ffbc83
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-10 00:48:18 -0300

    Exemplo sequencial - Sem goroutines

--- interfaces/main.go
+++ interfaces/main.go
@@ -5,34 +5,45 @@ import (
 	"time"
 )
 
+// ============================================
+// TRÊS MÉTODOS (cada um demora 1 segundo)
+// ============================================
 
-func FazerCafe(){
-	fmt.Println("Fazendo café")
-	time.Sleep(3*time.Second)
-	fmt.Println("Café pronto")
+func ValidarPedido(pedidoID int) string {
+	time.Sleep(1 * time.Second) //simula trabalho
+	return fmt.Sprintln("Pedido :",pedidoID ,"Validação concluída", pedidoID)
 }
 
-func FritarOvos(){
-	fmt.Println("Fritando Ovos")
-	time.Sleep(3*time.Second)
-	fmt.Println("ovos prontos")
+func ProcessarPedido(pedidoID int) string {
+	time.Sleep(1 * time.Second) //simula trabalho
+	return fmt.Sprintln("Pedido :",pedidoID, "Processamento concluído", pedidoID)
 }
 
-func FazerTorrada(){
-	fmt.Println("FazerTorrada")
-	time.Sleep(3*time.Second)
-	fmt.Println("Torrada Pronta")
+func NotificarCliente(pedidoID int) string {
+	time.Sleep(1 * time.Second) //simula trabalho
+	return fmt.Sprintln("Pedido ",pedidoID, ": Notificacão enviada")
 }
 
+// ============================================
+// EXECUÇÃO SEQUENCIAL (sem goroutines)
+// ============================================
 
 func main() {
 
-	start := time.Now()
-	go FazerCafe()
-	go FritarOvos()
-	go FazerTorrada()
+	inicio := time.Now()
+	pedidoID := 123
+	fmt.Println("Iniciando processamento sequencial....\n")
 
-	time.Sleep(3* time.Second)
-fmt.Println( time.Since(start))
+	//Executa um depois do outro
+	resultado1 := ValidarPedido(pedidoID)
+	fmt.Println(resultado1)
+
+	resultado2 := ProcessarPedido(pedidoID)
+	fmt.Println(resultado2)
+	
+	resultado3 := NotificarCliente(pedidoID)
+	fmt.Println(resultado3)
+
+	fmt.Println("Tempo total: ", time.Since(inicio))
 	fmt.Println("....FIM.....")
 }
