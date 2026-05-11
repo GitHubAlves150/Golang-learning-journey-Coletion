0b9b749ef120d90dbb6351b4569119a039e4bc51
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-11 17:25:40 -0300

    exemplo de mutex

--- interfaces/main.go
+++ interfaces/main.go
@@ -8,21 +8,11 @@ import (
 // channels
 // ============================================
 
-func Readmessage(ch <-chan string) {
-	msg1:= <-ch
-	msg2:= <-ch
-	fmt.Println("menssagem recebida: \n", msg1," e ", msg2)
-}
+
 
 func main() {
 
-	ch:=make(chan string, 2)
 
-
-	ch <-"Menssagem 1"
-	ch <-"Menssagem 2"
-
-	Readmessage(ch)
 
 	fmt.Println("....FIM.")
 }
