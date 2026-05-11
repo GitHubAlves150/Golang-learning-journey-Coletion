b72df9f74354e11ddb3d4c7b2b23039272e566a6
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-11 16:12:49 -0300

    não travou mesmo diminuindo o buffer

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,43 +2,28 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 // ============================================
 // channels
 // ============================================
 
+func Sendmessage(ch chan<- string) {
+	ch <- "Menssagem 1"
+	ch <- "Menssagem 2"
+	ch <- "Menssagem 3"
+
+}
+
 func main() {
 
-	ch := make(chan string, 3) //declaração do channel
+	ch := make(chan string,2)
 
-	go func()  {
-		fmt.Println("escrevendo no channel com buffer")
-		ch <- "menssagem 1"
-		fmt.Println("Menssagem 1 enviada")
-		ch <- "menssagem 2"
-		fmt.Println("Menssagem 2 enviada")
-		ch <- "menssagem 3"
-		fmt.Println("Menssagem 3 enviada")
-		ch <- "menssagem 4"
-		fmt.Println("Menssagem 4 enviada")
-	}()
+	go Sendmessage(ch)
 
-	time.Sleep(2 *time.Second)
-
-	go func ()  {
-		for i:=0; i<4;i++{
-			msg:= <-ch
-			fmt.Println("Recebido: ", msg)
-			time.Sleep(1*time.Second)
-		}
-
-	}()
-
-
-	time.Sleep(10*time.Second)
-
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 
 	fmt.Println("....FIM.")
 }
