3071cb957548777774e1511126d6c41623033a63
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-12 17:22:52 -0300

    Exemplo de race condition - sem Mutex

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,17 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 // ============================================
-// channels
+// Mutex
 // ============================================
 
+var T int = 100000
+var counter int = 0 //recurso compartilhado
 
+func IncrementCount(wg *sync.WaitGroup) {
+	defer wg.Done()
+	counter++
+}
 
 func main() {
 
+	var wg sync.WaitGroup
 
+	wg.Add(T)
+
+	fmt.Println("......", counter)
+
+	for i := 0; i < T; i++ {
+
+		go IncrementCount(&wg)
+
+		//fmt.Println("..", counter)
+
+	}
+
+	wg.Wait()
+	fmt.Println(".>>", counter)
 
 	fmt.Println("....FIM.")
 }
