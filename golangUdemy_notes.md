862c6bf518f1b395ca77d7c17493c5571820801b
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-16 17:49:46 -0300

    Exemplo básico de tratamento de erro

--- golangUdemy_notes.md
+++ golangUdemy_notes.md
@@ -1,23 +1,4 @@
-# 🎯 Exercício 3: Deadlock com Mutex
+# 🎯 tratamento de erros em golang
 
-## 🎯 Objetivo do Exercício
-Entender o que é DEADLOCK (abraceamento) - uma situação onde as goroutines ficam travadas para sempre esperando umas pelas outras.
+Os erros são retornados pelas funções  
 
-- Deadlock = "O programa trava e nunca mais sai"  
-
-
-## 📝 Enunciado
-
-Você tem duas contas bancárias e vai fazer uma transferência entre elas. Mas o código tem um erro que causa deadlock!
-
-Duas goroutines tentam transferir dinheiro uma para a outra ao mesmo tempo, cada uma segurando um mutex e esperando pelo outro.
-
-Sua missão: Identificar por que o programa trava e corrigir o deadlock.  
-
-
-## 🧪 Execute e Observe  
-## 🔍 O que está acontecendo?
-
-O programa quebrou porque duas goroutines tentaram escrever no map ao mesmo tempo.  
-
-**Remova os comentários e veja o resultado. Tenta debugar com F5 para ver o comportamento do código**
--- interfaces/main.go
+++ interfaces/main.go
@@ -1,60 +1,41 @@
 package main
 
-import(
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+import (
+	"errors"
+	"fmt"
 )
- 
-var votos = make(map[string]int)  // ← map compartilhado!
-var mu sync.Mutex
 
-
-func votar(wg *sync.WaitGroup) {
-    defer wg.Done()
-    mu.Lock()//Comente esta linha e clica no F5 para debugar linha por linha. Verás que o map vai ser cnsultado simultaneo
-    // Escolhe um candidato aleatório
-    candidatos := []string{"A", "B", "C"}
-    escolhido := candidatos[rand.Intn(3)]
-    
-    // ← PROBLEMA! Acessando map sem proteção
-    votos[escolhido]++
-       mu.Unlock()//Comente esta linha e clica no F5 para debugar linha por linha. Verás que o map vai ser cnsultado simultaneo
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("Erro!Não é possível dividri por zero")
+	}
+	return a / b, nil
 }
 
-func sum(s []int, c chan int) {
-      sum := 0
-       for _, v := range s {
-               sum += v
-       }
-       c <- sum // send sum to c
- }
- 
+func openFile(name string) error {
+	if name == "" {
+		return fmt.Errorf("Erro ao abrir o arquivo %w", errors.New("O nome do arquivos está vazio"))
+	}
+	return nil
 
- func main() {
-    
-    var wg sync.WaitGroup
-    numGoroutines := 1000
-    
-    inicio := time.Now()
-    
-    for i := 0; i < numGoroutines; i++ {
-        wg.Add(1)
-        go votar(&wg)
-    }
-    
-    wg.Wait()
-    
-    total := 0
-    for _, v := range votos {
-        total += v
-    }
-    
-    fmt.Printf("Esperado: %d votos\n", numGoroutines)
-    fmt.Printf("Total apurado: %d votos\n", total)
-    fmt.Printf("Distribuição: %+v\n", votos)
-    fmt.Printf("Tempo: %v\n", time.Since(inicio))
-    
- 
+}
+
+func main() {
+
+	result, erro := divide(10, 10)
+	if erro != nil {
+		fmt.Println("Erro! ", erro)
+		return
+	}
+	fmt.Println("Resultado", result)
+	fmt.Println("Erro", erro)
+
+	erro = openFile("")
+	if erro!=nil{
+		fmt.Println("não foi possível abrir o arquivo")
+		return
+	}
+
+	fmt.Println("Arquivo abrto com sucesso")
+	fmt.Println("....FIM....")
 }
