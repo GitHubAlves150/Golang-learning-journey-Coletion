2469282399e1aff8007a8cbbd6d5d14caacc35dd
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-19 18:04:43 -0300

    Padrão de em todas as APIS em go

--- interfaces/main.go
+++ interfaces/main.go
@@ -8,17 +8,20 @@ import (
 )
 
 type Summary struct {
-	ID          int    `json:"id"`
-	Description string `json:"description"`
+	ID          int    `json:"id"` //Quando converter para JSON, o campo ID vira "id"
+	Description string `json:"description"`//Quando converter para JSON, o campo Description vira "description"
 }
 
 var summaries []Summary
 
 func summariesHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Println("list summary")
-	//retornar um tipo de json
+	
+	//Define o cabeçalho: o que estamos enviando é um Json
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(summaries); err != nil {
+
+	//Converte a slice summaries para JSON e envia
+	if err := json.NewEncoder(w).Encode(summaries); err != nil {//json como resposta
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
@@ -27,9 +30,9 @@ func main() {
 
 	//Rotas
 	//C.R.U.D
-	http.HandleFunc("/summary", summariesHandler)
+	http.HandleFunc("/summary", summariesHandler)//cria uma rota
 	log.Println("Server is Running")
-	log.Fatal(http.ListenAndServe(":8080", nil)	)
+	log.Fatal(http.ListenAndServe(":8080", nil)	)//cria um servidor local
 
 
 	fmt.Println("...FIM...")
