b27e1db96a920e1ec41663488a144ea37dea2330
GitHubAlves150 <senoratec.io@gmail.com>
2026-05-20 11:02:54 -0300

    Servidor que envia json(método get)

--- interfaces/main.go
+++ interfaces/main.go
@@ -3,38 +3,43 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
+//==============================================================
+//Servidor que ENVIA JSON(GET)
+//==============================================================
 
-type Summary struct {
-	ID          int    `json:"id"` //Quando converter para JSON, o campo ID vira "id"
-	Description string `json:"description"`//Quando converter para JSON, o campo Description vira "description"
+// Pessoa é a estrutura de dados que vamos enviar
+type Pessoa struct {
+	Nome  string `json:"nome"`
+	Idade int    `json:"idade"`
 }
 
-var summaries []Summary
-
-
-func summariesHandler(w http.ResponseWriter, _ *http.Request) {
-	fmt.Println("list summary")
-	
-	//Define o cabeçalho: o que estamos enviando é um Json
-	w.Header().Set("Content-Type", "application/json")
-
-	//Converte a slice summaries para JSON e envia
-	if err := json.NewEncoder(w).Encode(summaries); err != nil {//json como resposta
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
 
 func main() {
 
-	//Rotas
-	//C.R.U.D
-	http.HandleFunc("/summary", summariesHandler)//cria uma rota
-	log.Println("Server is Running")
-	log.Fatal(http.ListenAndServe(":8080", nil)	)//cria um servidor local
+	//Cria uma estrutura do tipo Pessoa
+	people := Pessoa{
+		Nome:  "Lucas Lorenço Alves",
+		Idade: 38,
+	}
+
+	//Cria um Handler(manipulador) que retorna essa estrutura pessoa em formato JSON
+	http.HandleFunc("/people", func(w http.ResponseWriter, r *http.Request) {
+		//Diz que a resposta será um JSON
+		w.Header().Set("Contente-Type", "application/json")
+
+		//Converte a estrutura para formato JSON e envia
+		json.NewEncoder(w).Encode(people)
+
+		fmt.Println("✅ Enviei a estrutura")
+	})
+
+	fmt.Println("Servidor rodando")
+	fmt.Println("Acesse: http:/localhost:8080/people")
+
+	//Abre servidor
+	http.ListenAndServe(":8080", nil)
 
 
-	fmt.Println("...FIM...")
 }
