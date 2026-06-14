607a8ed08ff4949bfe5f0cb22f8492b4abcbcf00
Lucas Lorenço Alves <senoratec.io@gmail.com>
2026-06-14 11:51:37 -0300

    ETAPA 2: Múltiplas Implementações

--- main.go
+++ main.go
@@ -4,26 +4,55 @@ import(
     "fmt"
 )
 
-
-
-//Passo 1: Interface básica - Defiinir a interface (o contrato)
+//Mesma interface de branch Topics/interface_basica
 type Animal interface{
     EmitirSom() string
 }
 
-//Passo 2: Criar a struct (tipo concreto)
+//Implementação 1
 type Cachorro struct{
     Nome string
-} 
-
-//passo 3: Implementar o método da interface
-func (c Cachorro) EmitirSom() string{
-    return "Au-Au"
 }
+
+func (c Cachorro) EmitirSom() string{
+    return "Au-Au" + c.Nome
+}
+
+//Implementação 2
+type Gato struct{
+    Nome string
+}
+
+func (g Gato)EmitirSom()string{
+    return "Miau" + g.Nome
+}
+
+//Implementação 3
+type Vaca struct{
+    Nome string
+}
+
+func (v Vaca)EmitirSom()string{
+    return "Muuuu" + v.Nome
+}
+
+
+//Função que aceita qualquer animal
+func FazerBarulho(animal Animal){
+    fmt.Println(animal.EmitirSom())
+}
+
+
 
 func main(){
 
-    var a Animal  //Variavél do tipo interface
-    a = Cachorro{Nome: "Guapeca"}
-    fmt.Println("..", a.EmitirSom())
+    cachorro := Cachorro{Nome:"Dog 1"}
+    gato := Gato{Nome: "Cat 1"}
+    vaca:= Vaca{Nome: "Cow 1"}
+
+    //Todas funcionam por que implementam Animal
+    FazerBarulho(cachorro);
+    FazerBarulho(gato);
+    FazerBarulho(vaca);
+
 }
