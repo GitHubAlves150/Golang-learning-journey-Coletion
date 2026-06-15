14605612a5fb67849f9a2b4367f2a84913edacf4
Lucas Lorenço Alves <senoratec.io@gmail.com>
2026-06-15 19:37:27 -0300

    🟢 ETAPA 3: Goroutines no Rastreador (simulado) - Objetivo: Simular vários veículos enviando dados ao mesmo tempo.

--- main.go
+++ main.go
@@ -2,37 +2,44 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math/rand"
 	"sync"
+	"time"
 )
 
-func ProcessarVeiculo(id int, wg *sync.WaitGroup){
+// Simula um veiculo enviando dados de GPS
+func EnviandoVeiculos(veiculoID int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	fmt.Printf("\nProcessando veiculo: %d", id)
-	time.Sleep(1*time.Second)//simula trabalho
-	fmt.Printf("\nveiculo %d Processado\n", id)
+	//Simula 3 leituras de GPS
+	for leitura := 0; leitura <= 3; leitura++ {
+		lat := -23.5 + rand.Float64()
+		lon := -45.3 + rand.Float64()
+		velocidade := rand.Float64() * 100
+		fmt.Printf("\nVelocidade do veiculo %d [Leitura %d]: lat: %.4f, lon: %.4f, velocidade: %.1f km/h", veiculoID, leitura, lat, lon, velocidade)
+		time.Sleep(100 * time.Millisecond) //Simula intervalo entre as leituras
+	}
+	fmt.Printf("\nveiculo %d finalizou suas leituras.\n", veiculoID)
 
 }
 
-
-
 func main() {
 
-	fmt.Println("====PROCESSANDO VEICULO COM WAITGROUP======")
-
+	fmt.Println("=== RASTREADOR COM MÚLTIPLOS VEÍCULOS ===\n")
 	var wg sync.WaitGroup
 
-	//Processar 5 vecículo em paralelo
-	for i:=1; i<5; i++{
-		wg.Add(1) //DIz! mais uma gouroutine
-		go ProcessarVeiculo(i, &wg) //Passa o ponteiro do wg
+	numeroDeVeiculos := 3
+
+	//Inicia Varios veiculos em paralelos
+	for i := 0; i <= numeroDeVeiculos; i++ {
+		wg.Add(1) //diz quantas goo deve ser chamadas. Como está num FOR, entçao serṕa chamada uma por vez
+		go EnviandoVeiculos(i, &wg)
+		//time.Sleep(1 * time.Second) //Simula intervalo entre as leituras
+
 	}
 
-	fmt.Println("\nAguardanddo todos os veículos serem processados\n")
-	wg.Wait()// Bloqueia até todas gouroutines chamaram Done()
-
-	fmt.Println("\nTodos os gouroutines foram processados\n")
-
+	fmt.Printf("\nTodos os vecículos estçao enviando dados simultaneamente\n")
+	wg.Wait()
+	fmt.Println("\n🎉 Todos os veículos finalizaram o envio!\n")
 
 }
