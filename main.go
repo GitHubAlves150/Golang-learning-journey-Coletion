dbc01c3f4d535100ee4e5c3ea183a0800cc9d1ee
Lucas Lorenço Alves <senoratec.io@gmail.com>
2026-06-15 17:58:57 -0300

    🟢 ETAPA 2: WaitGroup (Sincronização)

--- main.go
+++ main.go
@@ -2,121 +2,37 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"time"
+	"sync"
 )
 
-//============================================
-//INTERFACE INDIVIDUAIS
-//============================================
+func ProcessarVeiculo(id int, wg *sync.WaitGroup){
+	defer wg.Done()
 
-type Localizavel interface {
-	ObterCoordenadas() (lat, lon float64)
+	fmt.Printf("\nProcessando veiculo: %d", id)
+	time.Sleep(1*time.Second)//simula trabalho
+	fmt.Printf("\nveiculo %d Processado\n", id)
+
 }
 
-type Velocidade interface {
-	ObterVelocidade() float64
-}
 
-type Direcao interface {
-	ObterDirecao() float64
-}
-
-// ============================================
-// COMPOSIÇÃO: Interface que COMBINA outras
-// ============================================
-// Rastreador completo tem TODOS os métodos das interfaces acima
-type RastreadorCompleto interface {
-	Localizavel //Tem ObterCoordenadas
-	Velocidade  //Tem ObterVelocidade
-	Direcao     //Tem ObterDireção
-
-	//Dá para adicionar mpetodos próprios
-	GetID() string
-}
-
-// ============================================
-// IMPLEMENTAÇÃO
-// ============================================
-// --Tipo comcreto
-type RastreadorReal struct {
-	ID         string
-	UltimaLat  float64
-	UltimaLon  float64
-	Velocidade float64
-	Direcao    float64
-}
-
-// Implementação
-func (r RastreadorReal) ObterCoordenadas() (float64, float64) {
-	return r.UltimaLat, r.UltimaLon
-}
-
-func (v RastreadorReal) ObterVelocidade() float64 {
-	return v.Velocidade
-}
-
-func (d RastreadorReal) ObterDirecao() float64 {
-	return d.Direcao
-}
-
-func (r RastreadorReal) GetID() string {
-	return r.ID
-}
-
-// ============================================
-// Funções que usam as interfaces
-// ============================================
-
-// Função que só precisa de localização
-func MostrarLocal(locallizavel Localizavel) {
-	lat, lon := locallizavel.ObterCoordenadas()
-	fmt.Printf("\nLocalização:%.4f, %.4f ", lat, lon)
-}
-
-// Função que precisa de tudo (rastreador completo)
-func MostrarStatusCompleto(rastrear RastreadorCompleto) {
-	fmt.Println("\n=====STATUS COMPLETO==========")
-	fmt.Printf("\nID: %s", rastrear.GetID())
-
-	lat, lon := rastrear.ObterCoordenadas()
-	fmt.Printf("\nPosição [%.4f - %.4f]", lat, lon)
-
-	fmt.Printf("\nvelocidade: %.1f", rastrear.ObterVelocidade())
-	fmt.Printf("\nDireção: %.1f", rastrear.ObterDirecao())
-	fmt.Printf(strings.Repeat("=", 50))
-}
 
 func main() {
-	rastreador := RastreadorReal{
-	ID:           "ES",
-	UltimaLat:   -32.2222,
-	UltimaLon:   -45.5555,
-	Velocidade:  34.2,
-	Direcao:     84.3,
+
+	fmt.Println("====PROCESSANDO VEICULO COM WAITGROUP======")
+
+	var wg sync.WaitGroup
+
+	//Processar 5 vecículo em paralelo
+	for i:=1; i<5; i++{
+		wg.Add(1) //DIz! mais uma gouroutine
+		go ProcessarVeiculo(i, &wg) //Passa o ponteiro do wg
 	}
 
-	//RastreadorReal implementa Localizavel (porque tem ObterCoordenadas)
-	MostrarLocal(rastreador)
+	fmt.Println("\nAguardanddo todos os veículos serem processados\n")
+	wg.Wait()// Bloqueia até todas gouroutines chamaram Done()
 
-	//RastreadorReal implementa RastreadorCompleto (tem todos os métodos)
-	MostrarStatusCompleto(rastreador)
+	fmt.Println("\nTodos os gouroutines foram processados\n")
 
-
-	// ========================================
-    // DEMONSTRAÇÃO DE FLEXIBILIDADE
-    // ========================================
-
-	fmt.Printf("\n--Flexibilidade: Funções aceitam diferentes interfaces --")
-
-	//Função que aceita Localizavel (pode ser qualquer coisa com coordenadas)
-	coordenadas := []Localizavel{
-        rastreador,
-         // struct anônima
-    }
-
-	for _, valores:= range coordenadas{
-		//fmt.Printf("|", valores)
-		MostrarLocal(valores)
-	}
 
 }
